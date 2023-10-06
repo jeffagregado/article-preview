a32aad3c8f4a20e3e68ce912d61886b0b8c43092
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2023-10-06 18:07:10 +0800

    js/index: add close tooltip outside element

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 const shareIcon = document.querySelector('.shareIcon');
 const tooltip = document.querySelector('.tooltip');
+const body = document.querySelector('body');
+const active = document.querySelector('.active');
 
 shareIcon.addEventListener('click', () => {
   tooltip.classList.toggle('active');
   shareIcon.classList.toggle('active');
 });
+
+body.addEventListener('click', (event) => {
+  if (!event.target.closest('.tooltip') && !event.target.closest('.shareIcon')) {
+    tooltip.classList.remove('active');
+    shareIcon.classList.remove('active');
+  }
+});
+
