b298f3f2de6b8f831411a64e22271642bb5ecd16
Jeffrey Andrew Agregado <jeffagregado@gmail.com>
2023-10-06 18:00:53 +0800

    removed  comments

--- js/index.js
+++ js/index.js
@@ -1,10 +1,3 @@
-// const shareIcon = document.querySelector('.shareIcon');
-// const hide = document.querySelector('.hide');
-
-// shareIcon.addEventListener('click', () => {
-//   hide.style.display = hide.style.display === 'none' ? 'flex' : 'none';
-// });
-
 const shareIcon = document.querySelector('.shareIcon');
 const tooltip = document.querySelector('.tooltip');
 
